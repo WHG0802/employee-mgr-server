48fafee1f59e52e88f3cce2a22eebc80aca55f22
William Guy <williamguy@Williams-Air.fios-router.home>
2022-07-16 09:35:46 -0400

    first commit

---
+++ src/main/java/com/guy/employeemgrserver/EmployeeMgrServerApplication.java
@@ -0,0 +1,13 @@
+package com.guy.employeemgrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMgrServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMgrServerApplication.class, args);
+	}
+
+}
